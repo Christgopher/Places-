ad71ab525a9d83f94335124bca0e8bf6aa831c76
cleri philiastides & chris le compte <student@epicodus.com>
2015-04-28 10:48:18 -0700

    Add and pass Place#save and Place.clear specs

--- lib/places.rb
+++ lib/places.rb
@@ -1,6 +1,6 @@
 class Place
+  @@all = []
   define_method(:initialize) do |description|
-    @@all = []
     @description = description
   end
 
@@ -11,4 +11,12 @@ class Place
   define_singleton_method(:all) do
     @@all
   end
+
+  define_method(:save) do
+    @@all.push(self)
+  end
+  define_singleton_method(:clear) do
+    @@all = []
+  end
+
 end
--- spec/places_spec.rb
+++ spec/places_spec.rb
@@ -3,14 +3,29 @@ require('places')
 
 describe(Place) do
   describe('#description') do
-    it("returns a description of Place") do
+    it("lets you give it a description") do
     test_place = Place.new("England")
     expect(test_place.description()).to(eq("England"))
     end
   end
   describe('.all') do
-    it("begins as an empty empty") do
+    it("begins as an empty array") do
     expect(Place.all()).to(eq([]))
     end
   end
+  describe('#save') do
+    it("adds a place to the array of saved places") do
+      test_place = Place.new("Australia")
+      test_place.save()
+      expect(Place.all()).to(eq([test_place]))
+    end
+  end
+  describe('.clear') do
+    it("empties out all the saved places") do
+      Place.new("France").save()
+      Place.clear()
+      expect(Place.all()).to(eq([]))
+    end
+  end
+
 end
